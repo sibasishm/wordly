f61594b6a8a0fbc6d48025a1e14aed8d17e1ab60
Sibasish Mohanty <smsibasish@gmail.com>
2020-09-06 18:10:59 +0530

    word cards styled

--- components/WordsContainer.js
+++ components/WordsContainer.js
@@ -1,5 +1,12 @@
 import { useQuery } from 'react-query';
 
+const colorsMap = {
+	noun: 'text-green-700 bg-green-100',
+	verb: 'text-pink-700 bg-pink-100',
+	adjective: 'text-indigo-700 bg-indigo-100',
+	adverb: 'text-orange-700 bg-orange-100',
+};
+
 async function fetchWords() {
 	const res = await fetch('/api/words');
 	const data = await res.json();
@@ -16,9 +23,25 @@ export default function WordsContainer() {
 	if (status === 'error') return <p>{error.message}</p>;
 
 	return (
-		<>
-			<p>Found {words.length} words.</p>
-			<pre>{JSON.stringify(words, null, 2)}</pre>
-		</>
+		<div className='flex flex-wrap -mx-2'>
+			{words.map(word => (
+				<article className='w-full sm:w-1/2 lg:w-1/3 px-2'>
+					<div className='mb-4 border bg-white rounded p-4 md:px-6'>
+						<p
+							className={`text-sm p-1 px-2 rounded-full inline-flex ${
+								colorsMap[word.type]
+							}`}
+						>
+							{word.type}
+						</p>
+						<h2 className='font-bold text-xl md:text-2xl lowercase'>
+							{word.name}
+						</h2>
+						<p className='text-md text-gray-700'>{word.meaning}</p>
+						<p className='mt-2 text-sm text-gray-700'>{word.sentence}</p>
+					</div>
+				</article>
+			))}
+		</div>
 	);
 }
