2819420f17ad970b3b5b7ce26611099c5e1d3070
Sibasish Mohanty <smsibasish@gmail.com>
2020-09-08 16:44:20 +0530

    change of plans

--- components/WordsContainer.tsx
+++ components/WordsContainer.tsx
@@ -7,20 +7,20 @@ async function fetchWords() {
 	return data;
 }
 
-export default function WordsContainer() {
+export default function WordsContainer(): React.ReactNode {
 	const { data: words, error, status } = useQuery('words', fetchWords, {
 		refetchOnWindowFocus: false,
 	});
-	console.log(error);
 
 	if (status === 'loading') return <p>Loading...</p>;
-	if (status === 'error') return <p>{error.message}</p>;
+	if (status === 'error')
+		return <p>Something went wrong. Please refresh this page.</p>;
 
 	return (
 		<div className='flex flex-wrap -mx-2'>
 			{words.map(word => (
-				<article className='w-full sm:w-1/2 lg:w-1/3 px-2'>
-					{<Word word={word} />}
+				<article className='w-full sm:w-1/2 lg:w-1/3 px-2' key={word.id}>
+					<Word word={word} />
 				</article>
 			))}
 		</div>
